4ceddd98714096c7b64dde6f9df11faf06925e54
soumyadeep <samantasoumya284@gmail.com>
2026-01-28 18:06:18 +0530

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require("express")
 
 const app=express() //server instance created
 app.get('/',(req,res)=>{
-    res.send("hello duggu muggu")
+    res.send("hello duggu mugguu")
 })
 app.get("/about",(req,res)=>{
     res.send("this is about page and i am going to learn about backend")
